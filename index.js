057d28816b5f84d84ca01bd2c9bfa1adaad0ed51
Jorge Luis Adco Ccahuay <jorgead0812@gmail.com>
2022-07-15 17:26:29 -0500

    add chat simple socket io

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { uuid } = require('uuidv4');
+
 //Servidor de express
 const express = require('express');
 
@@ -12,12 +14,20 @@ const io = require('socket.io')(server);
 //Desplegamos el directorio publico
 app.use(express.static(__dirname + '/public'));
 const messages = [];
+const users = [];
 io.on('connection', (socket) => {
-  console.log('Cliente conectado');
   socket.emit('message-current', messages);
   socket.on('message-to-server', (data) => {
     messages.push(data);
     io.emit('message-from-server', messages);
+  });
+  socket.on('join-chat', (room) => {
+    socket.join(room);
+    console.log(`user with id: ${socket.id} joined room ${room}`);
+  });
+  socket.on('add-user-to-server', (user) => {
+    users.push(user);
+    io.emit('users-from-server', users);
   });
 });
 
